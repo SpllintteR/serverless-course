67c04fdb62b4f50b9443a7ab9bee8824e9bff3a8
paulo.weber <paulo.weber@senior.com.br>
2021-04-06 21:33:14 -0300

    save picture on auction
    validate only seller can update auction picture

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -3,18 +3,26 @@ import httpErrorHandler from '@middy/http-error-handler';
 import createError from 'http-errors';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from './lib/uploadPictureToS3';
+import { updateAuctionPicture } from './lib/updateAuctionPicture';
 
 export async function uploadAuctionPicture(event) {
     const {id} = event.pathParameters;
+    const { email } = event.requestContext.authorizer;
     const auction = await getAuctionById(id);
+
+    if (auction.seller != email) {
+        throw new createError.Forbidden(`Only the seller can update the auction picture`);
+    }
+
     const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
     const buffer = Buffer.from(base64, 'base64');
 
     try {
-        const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
+        const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+        const updatedAuction = await updateAuctionPicture(auction.id, pictureUrl);
         return {
             statusCode: 200,
-            body: JSON.stringify(uploadToS3Result)
+            body: JSON.stringify(updatedAuction)
         };
     } catch (error) {
         console.log(error);
